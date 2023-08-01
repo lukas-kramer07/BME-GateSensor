09ea2f14d5a97cb8f59f1e82b31d957abdfec349
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-08-01 19:08:28 +0200

    cc

--- GateSensor.ino
+++ GateSensor.ino
@@ -1,3 +1,9 @@
+/*/
+Lukas Kramer
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+/*/
+
 // Import the necessary libraries
 #include <ESP8266WiFi.h>  // WiFi Library
 #include <ESPAsyncTCP.h>  // Libraries for async Webserver
